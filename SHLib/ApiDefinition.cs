b36b0707d2dc308531466ba083c0b24f9f019aa7
sahil.khanna <sahil.khanna@kochartech.com>
2019-01-08 13:36:04 +0530

    Initial import

---
+++ SHLib/ApiDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace SHLib
+{
+
+    // @interface MyFirstFramework : NSObject
+    [BaseType(typeof(NSObject))]
+    [Protocol]
+    interface MyFirstFramework
+    {
+        // +(void)launch;
+        [Static]
+        [Export("launch")]
+        void Launch();
+
+        // +(NSString * _Nonnull)print __attribute__((warn_unused_result));
+        [Static]
+        [Export("print")]
+        string Print { get; }
+    }
+}
+
---
+++ SHLib/Structs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+//public static class MyFirstFramework {
+
+//    [DllImport ("@rpath/MyFirstFramework.framework/MyFirstFramework")]
+//    static extern IntPtr functionInFramework ();
+//}
